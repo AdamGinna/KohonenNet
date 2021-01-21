605b0990b9aa0fd9088919bfafd0b16476ef2394
adamuss <adam.ginna.work@gmail.com>
2021-01-21 12:34:48 +0100

    Kohonen init

---
+++ kohonen.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+class Kohonen(object):
+
+	def __init__(self, h, w,dim):
+		self.shape = (h,w,dim) # dim-przestrzen danych
+		#self.som= np.random.random(self.shape) # np.random.random - zadanie
+		self.som= np.array([[ (j/h, i/w) for i in range(w) ] for j in range(h)])
+		self.data = []
+		self.alpha0 = 0.5
+		self.sigma = 1.2
+		self.lamb = 300
+
+		self.steps = 300
+		self.step = 0
+
+	def train(self, training_data): # wyrzuca koncowa postac siatki a nie pojedyncza zmiane (nie da rady zrobic animacji)
+		self.data = training_data
+		for t in range(self.steps):
+			index = np.random.choice(range(len(self.data)))
+
+			best_neuron = self.find_best_neuron(self.data[index])
+			self.update_som(best_neuron, self.data[index], t)
+
+	def train_step(self, training_data): # wyrzuca koncowa postac siatki a nie pojedyncza zmiane (nie da rady zrobic animacji)
+		self.data = training_data
+		index = np.random.choice(range(len(self.data)))
+		best_neuron = self.find_best_neuron(self.data[index])
+		self.update_som(best_neuron, self.data[index], self.step)
+		self.step += 1
+
+
+	def find_best_neuron(self, start):
+		neuron_list = []
+		for x in range(self.shape[0]):
+			for y in range(self.shape[1]):
+				self.som[x, y]
+				dist = np.linalg.norm(start - self.som[x, y])
+				neuron_list.append(((x,y),dist))
+
+		neuron_list.sort(key=lambda x:x[1])
+		return neuron_list[0][0]
+
+	def update_som(self, best_neuron, data_point, step):
+		for x in range(self.shape[0]):
+			for y in range(self.shape[1]):
+				dist_to_best_neuron = np.linalg.norm(np.array(best_neuron) - np.array([x,y]))
+				self.update_cell((x,y), data_point, step, dist_to_best_neuron)
+
+	def update_cell(self, cell, data_point, t, dist):
+		self.som[cell] += self.alpha(t) * self.G(dist)*(data_point - self.som[cell])
+
+	def alpha(self, t):
+		return self.alpha0 * np.exp(-t/self.lamb)
+  
+	def G(self, dist):
+		return np.exp(-dist**2/(2*self.sigma**2))
